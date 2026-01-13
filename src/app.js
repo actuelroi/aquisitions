06a0b1410af70e1e9384161088102632e6c8cd70
actuelroi <yebouareel@gmail.com>
2026-01-13 20:02:45 +0100

    start

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.status(200).send('Hello from the index with in src folder ')
+})
+
+
+export default app;

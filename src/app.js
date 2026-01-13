2742cf5cf7b38089c30b44a6fb814f8056efb315
actuelroi <yebouareel@gmail.com>
2026-01-13 20:24:09 +0100

    add prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-import express from 'express'
+import express from 'express';
 
+const app = express();
 
-const app = express()
-
-app.get('/',(req,res)=>{
-    res.status(200).send('Hello from the index with in src folder ')
-})
-
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from the index with in src folder ');
+});
 
 export default app;
